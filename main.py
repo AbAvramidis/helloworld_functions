263b3948979c50c8296521460f99a821fc7be663
AbAvramidis <ab.avramidis@cloudsolutions.co.uk>
2020-05-11 11:23:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
-  res.status(200).send(message);
-};
+def hello_world(request):
+
+    return 'Hello,The Test!\n'
